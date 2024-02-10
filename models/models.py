857013a3a07a140cbcec4d095a830aca26fe1f03
DmAlFat <fateev8292@gmail.com>
2024-02-11 01:28:59 +0300

    add auth's block

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String, JSON
+from sqlalchemy import MetaData, Table, Column, Integer, String, JSON, Boolean
 
 metadata = MetaData()
 
@@ -8,7 +8,10 @@ user = Table(
     Column('id', Integer, primary_key=True),
     Column('username', String, nullable=False),
     Column('email', Integer, nullable=False),
-    Column('password', String, nullable=False),
+    Column('hashed_password', String, nullable=False),
     Column('referral_code', String),
     Column('referrals', JSON),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
