88e14b2600d63f0291d00f6168eb6d871a0985a4
DmAlFat <fateev8292@gmail.com>
2024-02-11 02:32:14 +0300

    fix migrations

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi_users import FastAPIUsers
-
 from fastapi import FastAPI, Depends
-
 from auth.auth import auth_backend
 from auth.database import User
 from auth.manager import get_user_manager
